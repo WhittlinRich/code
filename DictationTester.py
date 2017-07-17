e1c1b440628a84be40b0bae3d877bed914c3b3de
Rich <richemail@gmail.com>
2017-07-17 19:26:47 -0400

    Create DictationTester.py

---
+++ DictationTester.py
@@ -0,0 +1,31 @@
+import speech
+
+print('\n\nSay words.')
+
+with open('DictationTesterInput.txt', 'r') as file:
+	wordList = file.readlines()
+
+totalCorrect = 0
+totalWrong = 0
+totalWords = len(wordList)
+isMatched = False		
+						
+for word in wordList:
+	isMatched = False
+	while not isMatched:
+		speech.say(word)
+		userInput = input("\nSay " + word.strip() + ": ")
+		if userInput == word.strip():
+			isMatched = True
+			totalCorrect += 1
+			print("Correct!")
+		else:
+			totalWrong += 1
+			print("Not a match. Try Again.")
+		
+
+print("Your score: {0} out of {1}".format(totalCorrect, totalWords))
+print(round((totalCorrect/totalWords) * 100), "%")
+if(totalCorrect == totalWords):
+	print("Perfect score!")
+	
